cb7ba6f73728daf58342085247fa2280a4681321
Leonardo Pérez Marulanda <leono286@gmail.com>
2017-11-10 13:55:18 -0500

    changed ChuckNorrisJokes interface for readability

--- src/app/ChuckNorrisJokes.interface.ts
+++ src/app/ChuckNorrisJokes.interface.ts
@@ -1,10 +1,8 @@
 export interface ChuckNorrisJokes {
   type: string;
-  value: JokeValue;
-}
-
-interface JokeValue {
-  id: number;
-  joke: string;
-  categories: any[];
+  value: {
+    id: number;
+    joke: string;
+    categories: any[];
+  }
 }
